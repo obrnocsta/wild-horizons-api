4305f5fcfb0ddb85dd19b422e79b3dfd53d0d8cc
Bruno Costa <obrnocsta@gmail.com>
2025-11-25 10:29:53 -0300

    feat: adiciona rota não encontrada

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const server = http.createServer(async (req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify(data))
+  } else {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ error: 'not found', message: 'The requested route does not exist' }))
   }
 })
 
