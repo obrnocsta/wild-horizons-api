b9653230415968ef761a30757666fb7c95e8b08d
Bruno Costa <obrnocsta@gmail.com>
2025-11-25 15:53:42 -0300

    feat: adiciona filtro query params em função util 'filterQueryData()'

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import http from 'node:http'
 import { getDataFromDatabase } from './database/database.js'
 import { sendJSON } from './utils/sendJSON.js'
+import { filterData } from './utils/filterData.js'
+import { filterQueryData } from './utils/filterQueryData.js'
 
 const port = 8000
 
 const server = http.createServer(async (req, res) => {
   let data = await getDataFromDatabase()
 
-  if (req.url === '/api' && req.method === 'GET') {
+  const path = new URL(req.url, `http://${req.headers.host}`)
+
+  if (path.pathname === '/api' && req.method === 'GET') {
+    data = filterQueryData(data, path)
     sendJSON(res, 200, data)
 
   } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
