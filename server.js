0e0b0fb4b53c0b88618908e010b3d576e7148a0b
Bruno Costa <obrnocsta@gmail.com>
2026-03-21 13:26:17 -0300

    refactor: refaz a lógica de busca do parâmetro na rota '/api/continent', adiciona filtro de espaços no path da url

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
-let data = await getDataFromDB();
+const data = await getDataFromDB();
 
 const server = http.createServer((req, res) => {
   if (req.url === "/api" && req.method === "GET") {
@@ -12,13 +12,14 @@ const server = http.createServer((req, res) => {
     res.end(JSON.stringify(data));
   } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
     const params = req.url.split("/");
-    const continent = params.pop();
-    data = data.filter((d) =>
+    const continent = params.pop().replace("%20", " ");
+    console.log(continent);
+    const dataFiltered = data.filter((d) =>
       d.continent.toLowerCase().includes(continent.toLowerCase()),
     );
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(data));
+    res.end(JSON.stringify(dataFiltered));
   } else {
     res.statusCode = 404;
     res.setHeader("Content-Type", "application/json");
