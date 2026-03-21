18ab15f3bb356217db2b72d94bc2642b1e6e8204
Bruno Costa <obrnocsta@gmail.com>
2026-03-21 04:16:18 -0300

    feat: server dados 'mock' e responde com 'JSON.stringify' na rota '/api'

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
+const data = await getDataFromDB();
+
 const server = http.createServer((req, res) => {
   if (req.url === "/api" && req.method === "GET") {
-    res.end("Hello from the server!");
+    res.end(JSON.stringify(data));
   }
 });
 
