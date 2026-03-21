5539bdd00f7b87dc752341feb55a142c8557f293
Bruno Costa <obrnocsta@gmail.com>
2026-03-21 03:59:34 -0300

    feat: adiciona rota '/api' verificando o objeto 'req.url' e 'req.method'

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ import http from "node:http";
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  if (req.url === "/api" && req.method === "GET") {
+    res.end("Hello from the server!");
+  }
 });
 
 server.listen(PORT, () => {
