67ba18cc3f5c821cfa79fa0e199e925fc5fff844
Bruno Costa <obrnocsta@gmail.com>
2026-03-21 03:43:25 -0300

    feat: cria um servidor simples com o módulo 'http' do node na porta '8000'

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("We have a package.json!");
+import http from "node:http";
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on: http://localhost:${PORT}`);
+});
